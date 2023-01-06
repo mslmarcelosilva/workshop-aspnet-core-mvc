1f6795c490c8e9b1499e054589f6966ba271f44f
Marcelo Silva <mslmarcelosilva@gmail.com>
2023-01-05 22:14:30 -0300

    Validation

--- SalesWebMvc/SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/SalesWebMvc/Models/Seller.cs
@@ -9,15 +9,22 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         [Display(Name = "Nome")]
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Tamanho do {0} deve ter entre {2} e {1}")]
         public string Name { get; set; }
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [EmailAddress(ErrorMessage ="E-mail obrigatório")]
         public string Email { get; set; }
         [Display(Name = "Data de Aniversário")]
+        [Required(ErrorMessage = "{0} obrigatório")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
         [Display(Name = "Salário Base")]
-        [DisplayFormat(DataFormatString ="{0:F2}")]
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [Range (100.0, 5000.0, ErrorMessage = "{0} deve ser entre {1} e {2}")]
+        [DisplayFormat(DataFormatString ="R$ "+"{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
